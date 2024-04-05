4eeb380dfb4f24923b3fd40a26cec83dc7cb047e
fraugho <aghoanebom@gmail.com>
2024-04-05 00:26:10 -0800

    got requests to work and added logo

--- shadcn/frontend/src/app/login/page.tsx
+++ shadcn/frontend/src/app/login/page.tsx
@@ -3,12 +3,11 @@ import { Login } from "@/app/login/login";
 
 export default function Page() {
     return (
-        <body className="overflow-hidden">
-            <h1 className="text-center p-4">Void Pixel</h1>
-            <div className="flex justify-center items-center min-h-screen">
+        <body className="overflow-hidden flex justify-center items-center min-h-screen bg-black">
+            <div className="flex flex-col justify-center items-center min-h-screen">
+                <img src="http://127.0.0.1:8080/static/SVG/void_pixel.svg" className="min-h-20 mb-3" />
                 <Login />
             </div>
         </body>
     );
 }
-
